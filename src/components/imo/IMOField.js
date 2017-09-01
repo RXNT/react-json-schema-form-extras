a3a5d0da019853ae6e1f843b28492807507fbffc
Anton Oparin <mavarazy@gmail.com>
2017-09-01 15:10:30 +0300

    #15

--- src/components/imo/IMOField.js
+++ src/components/imo/IMOField.js
@@ -3,13 +3,13 @@ import IMOModal from "./IMOModal";
 import Modal from "react-bootstrap-modal";
 
 const AddButton = ({ onAdd }) => (
-  <div className="btn btn-success" onClick={onAdd}>
+  <div className="btn btn-success btn-sm" onClick={onAdd}>
     <i className="glyphicon glyphicon-plus" /> Add
   </div>
 );
 
 const FreeTextButton = ({ onAdd }) => (
-  <a className="btn btn-success" onClick={onAdd}>
+  <a className="btn btn-success btn-sm" onClick={onAdd}>
     <i className="glyphicon glyphicon-list-alt" /> Free text
   </a>
 );
