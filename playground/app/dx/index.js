26bf4bc8f4e47c1ecc11e353c2ec22c5acce84e7
Anton Oparin <mavarazy@gmail.com>
2017-09-18 18:54:07 +0300

    Adding DX tab for testing

---
+++ playground/app/dx/index.js
@@ -0,0 +1,11 @@
+import schema from "./schema";
+import uiSchema from "./uiSchema";
+import formData from "./formData";
+import externalFieldInstanceData from "./externalFieldInstanceData";
+
+export default {
+  schema,
+  uiSchema,
+  formData,
+  externalFieldInstanceData,
+};
