afc65ad70cd0aaf7fc3f34ac19655de2938fd7af
Anton Oparin <mavarazy@gmail.com>
2017-09-19 19:50:04 +0300

    fix #29

---
+++ playground/app/typeahead/externalFieldInstanceData.js
@@ -0,0 +1,7 @@
+// This is here to enable IMO part of integration project
+
+export default {
+  table: { type: "table" },
+  compositeArray: { type: "compositeArray" },
+  typeaheadOptions: { type: "typeaheadOptions" },
+};
--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 import AsyncComplexTypeaheadField from "./asyncComplexTypeaheadField";
 import TableField from "./TableField";
 import AsyncTypeaheadField from "./AsyncTypeaheadField";
+import TypeaheadField from "./TypeaheadField";
 import CompositeArrayField from "./CompositeArrayField";
 import IMOTypeaheadField from "./imo/IMOField";
 
@@ -8,6 +9,7 @@ export default {
   AsyncComplexTypeaheadField,
   table: TableField,
   typeahead: AsyncTypeaheadField,
+  typeaheadOptions: TypeaheadField,
   compositeArray: CompositeArrayField,
   imo: IMOTypeaheadField,
 };
