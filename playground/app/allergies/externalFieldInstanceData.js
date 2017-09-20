57decdf4ab4d1e3739d3896f52c8e42ae8d42360
Anton Oparin <mavarazy@gmail.com>
2017-09-20 14:34:30 +0300

    more clean up

--- playground/app/allergies/externalFieldInstanceData.js
+++
@@ -1,8 +0,0 @@
-// This is here to enable IMO part of integration project
-
-export default {
-  table: { type: "table" },
-  typeahead: { type: "typeahead" },
-  compositeArray: { type: "compositeArray" },
-  imo: { type: "imo" },
-};
--- playground/app/medications/index.js
+++ playground/app/medications/index.js
@@ -1,7 +1,4 @@
 import schema from "./schema.json";
 import uiSchema from "./uiSchema.json";
-import externalFieldInstanceData from "./externalFieldInstanceData";
 
-export default Object.assign({}, schema, uiSchema, {
-  externalFieldInstanceData,
-});
+export default Object.assign({}, schema, uiSchema);
