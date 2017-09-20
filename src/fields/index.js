956e02c3dd51c98dafbdd57b24d439e45f525c4d
Anton Oparin <mavarazy@gmail.com>
2017-09-20 14:31:00 +0300

    clean up

--- src/fields/index.js
+++ src/fields/index.js
@@ -1,4 +1,3 @@
-import AsyncComplexTypeaheadField from "./asyncComplexTypeaheadField";
 import TableField from "./TableField";
 import AsyncTypeaheadField from "./AsyncTypeaheadField";
 import TypeaheadField from "./TypeaheadField";
@@ -6,7 +5,6 @@ import CompositeArrayField from "./CompositeArrayField";
 import IMOTypeaheadField from "./imo/IMOField";
 
 export default {
-  AsyncComplexTypeaheadField,
   table: TableField,
   typeahead: AsyncTypeaheadField,
   typeaheadOptions: TypeaheadField,
