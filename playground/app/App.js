3d88002e0348693b79f3be1bb371c9b415ae4769
Anton Oparin <mavarazy@gmail.com>
2017-09-20 14:23:44 +0300

    #30

--- playground/app/App.js
+++ playground/app/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import applyExtras from "../../src/index";
 import Form from "react-jsonschema-form";
 
+import codes from "./codes";
 import typeahead from "./typeahead";
 import medications from "./medications";
 import imo from "./imo";
@@ -17,6 +18,7 @@ export function App() {
   let handleChange = ({ formData }) => console.log(JSON.stringify(formData));
   return (
     <div>
+      <FormWithExtras {...codes} onChange={handleChange} />
       <FormWithExtras {...typeahead} onChange={handleChange} />
       <FormWithExtras {...dx} onChange={handleChange} />
       <FormWithExtras {...functionalStatus} onChange={handleChange} />
---
+++ playground/app/codes/uiSchema.js
@@ -0,0 +1,19 @@
+export default {
+  coding: {
+    classNames: "col-md-12",
+    "ui:field": "compositeArray",
+    inputField: "typeahead",
+    arrayField: "table",
+    typeahead: {
+      url:
+        "/EHRV8PatientEncounterAPIServices/ehrv8/encounter/SearchProcedureCodes",
+      optionsMapping: "ProcedureCodes",
+      responseSchemaMapping: {
+        Code: "code",
+        Description: "description",
+      },
+      minLength: 1,
+      labelKey: ({ Code, Description }) => `${Code} - ${Description}`,
+    },
+  },
+};
