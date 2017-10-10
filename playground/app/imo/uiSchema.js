69982b8291a7f96ac09d8e0404e22f0cabe9de24
Anton Oparin <mavarazy@gmail.com>
2017-10-10 17:06:18 +0300

    Updating playground

--- playground/app/imo/uiSchema.js
+++ playground/app/imo/uiSchema.js
@@ -3,17 +3,20 @@ export default {
     classNames: "col-md-12",
     "ui:field": "collapsible",
     collapse: {
-      field: "compositeArray",
+      field: "ObjectField",
     },
-    inputField: "imo",
-    arrayField: "table",
-    table: {
-      tableCols: [
-        { dataField: "icd10", editable: false },
-        { dataField: "icd10Description", editable: false },
-        { dataField: "icd9", editable: false },
-        { dataField: "snomed", editable: false },
-      ],
+    fx: {
+      "ui:field": "compositeArray",
+      inputField: "imo",
+      arrayField: "table",
+      table: {
+        tableCols: [
+          { dataField: "icd10", editable: false },
+          { dataField: "icd10Description", editable: false },
+          { dataField: "icd9", editable: false },
+          { dataField: "snomed", editable: false },
+        ],
+      },
     },
   },
 };
