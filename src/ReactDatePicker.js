369c410f06a478888f9f19d857076e1d0a7b8c08
Anton Oparin <mavarazy@gmail.com>
2017-12-18 15:40:24 +0100

    Small bug fixes

--- src/ReactDatePicker.js
+++ src/ReactDatePicker.js
@@ -3,6 +3,18 @@ import PropTypes from "prop-types";
 import DayPickerInput from "react-day-picker/DayPickerInput";
 import { formatDate } from "react-day-picker/moment";
 
+const DEFAULT_UPDATE_DELAY = 100;
+
+function normalizeDay(day, format) {
+  if (day === undefined) {
+    return undefined;
+  } else if (format === "date-time") {
+    return day.toISOString();
+  } else if (format === "date") {
+    return day.toISOString().substr(0, 10);
+  }
+}
+
 export default class ReactDatePicker extends Component {
   constructor(props) {
     super(props);
@@ -21,18 +33,23 @@ export default class ReactDatePicker extends Component {
   handleKeyDown = evt => {
     if (evt.keyCode === 13) {
       this.refs.datePicker.getInput().blur();
+      this.notifyChange();
     }
   };
 
   handleBlur = () => {
+    let {
+      uiSchema: { rdp: { updateDelay = DEFAULT_UPDATE_DELAY } = {} } = {},
+    } = this.props;
+    setTimeout(this.notifyChange, updateDelay);
+  };
+
+  notifyChange = () => {
     let day = this.day;
-    let { schema: { format = "date-time" }, onChange } = this.props;
-    if (day === undefined) {
-      onChange(undefined);
-    } else if (format === "date-time") {
-      onChange(day.toISOString());
-    } else if (format === "date") {
-      onChange(`${day.toISOString().substr(0, 10)}`);
+    let { schema: { format = "date-time" }, onChange, formData } = this.props;
+    let event = normalizeDay(day, format);
+    if (event !== formData) {
+      onChange(event);
     }
   };
 
