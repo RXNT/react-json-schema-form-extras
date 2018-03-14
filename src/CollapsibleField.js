72b07678fbe4cd12eff26fade857f4964271f5af
Samip <sneu012@gmail.com>
2018-03-14 16:48:38 -0400

    Fixed issue with NaN on table if value is not present && state stored in collapsible

--- src/CollapsibleField.js
+++ src/CollapsibleField.js
@@ -194,6 +194,8 @@ class CollapsibleField extends Component {
     let { collapsed, AddElement } = this.state;
     let { collapse: { field } } = uiSchema;
     let CollapseElement = fields[field];
+    // uischema retains the value form the state
+    uiSchema.collapse.collapsed = this.state.collapsed;
 
     title = uiSchema["ui:title"] ? uiSchema["ui:title"] : title ? title : name;
     let customizedId = collapsed ? $id : undefined;
