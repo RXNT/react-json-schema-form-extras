2772205eaa4a89ee2df60bd5aa6be3d7ab27668b
Soffia Garg <soffia@rxnt.com>
2020-11-06 11:12:39 +0530

    ED-5361: update table UI for diagnosis

--- playground/app/dx/uiSchema.js
+++ playground/app/dx/uiSchema.js
@@ -1,16 +1,41 @@
 export default {
   diagnosis: {
-    classNames: "col-md-12",
-    "ui:field": "table",
+    classNames: 'col-md-12',
+    'ui:field': 'table',
     table: {
       tableCols: [
-        { dataField: "description", editable: false },
-        { dataField: "icd10", editable: false },
-        { dataField: "icd9", editable: false },
+        {
+          dataField: 'icd10',
+          editable: false,
+          className: 'col-md-2 tableStyle headerStyle',
+          columnClassName: 'col-md-2 tableStyle rowStyle',
+        },
+        {
+          dataField: 'description',
+          editable: false,
+          className: 'col-md-7 tableStyle headerStyle',
+          columnClassName: 'col-md-7 tableStyle rowStyle',
+        },
+
+        {
+          dataField: 'snomed',
+          editable: false,
+          className: 'col-md-2 tableStyle headerStyle',
+          columnClassName: 'col-md-2 tableStyle rowStyle',
+        }
+      ],
+      rightActions: [
+        {
+          "action": "delete",
+          className: 'table-action',
+          columnClassName: 'table-action',
+          editColumnClassName: 'table-action',
+          icon: 'glyphicon glyphicon-option-vertical'
+        },
       ],
     },
     imo: {
       freeText: false,
     },
   },
-};
+}
