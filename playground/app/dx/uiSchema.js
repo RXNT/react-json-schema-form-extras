d8472c38f09f6fa4b35929c96be605a8dbd2caff
Soffia Garg <soffia@rxnt.com>
2020-11-11 11:29:29 +0530

    ED-5361: update css

--- playground/app/dx/uiSchema.js
+++ playground/app/dx/uiSchema.js
@@ -1,38 +1,56 @@
 export default {
   diagnosis: {
-    classNames: 'col-md-12',
+    classNames: 'col-md-12 ',
     'ui:field': 'table',
     table: {
       tableCols: [
         {
           dataField: 'icd10',
           editable: false,
-          className: 'col-md-2 tableStyle headerStyle',
-          columnClassName: 'col-md-2 tableStyle rowStyle',
+          className: 'col-md-2 rxntTableStyle rxntHeaderStyle',
+          columnClassName: 'col-md-2 rxntTableStyle rxntRowStyle',
         },
         {
           dataField: 'description',
           editable: false,
-          className: 'col-md-7 tableStyle headerStyle',
-          columnClassName: 'col-md-7 tableStyle rowStyle',
+          className: 'col-md-7 rxntTableStyle rxntHeaderStyle',
+          columnClassName: 'col-md-7 rxntTableStyle rxntRowStyle',
         },
 
         {
           dataField: 'snomed',
           editable: false,
-          className: 'col-md-2 tableStyle headerStyle',
-          columnClassName: 'col-md-2 tableStyle rowStyle',
+          className: 'col-md-2 rxntTableStyle rxntHeaderStyle',
+          columnClassName: 'col-md-2 rxntTableStyle rxntRowStyle',
         }
       ],
       rightActions: [
         {
-          "action": "delete",
-          className: 'table-action',
-          columnClassName: 'table-action',
-          editColumnClassName: 'table-action',
-          icon: 'glyphicon glyphicon-option-vertical'
-        },
-      ],
+          action: "inheritedAction",
+          className: "col-md-1",
+          columnClassName: "col-md-1",
+          editColumnClassName: "col-md-1",
+          icon: "glyphicon glyphicon-option-vertical",
+          inheritedAction: [
+            {
+              action: "edit",
+              className: "col-md-1",
+              columnClassName: "col-md-1",
+              editColumnClassName: "col-md-1",
+              icon: "glyphicon glyphicon-pencil",
+              displayName: "Edit"
+            },
+            {
+              action: "delete",
+              className: "col-md-1",
+              columnClassName: "col-md-1",
+              editColumnClassName: "col-md-1",
+              icon: "glyphicon glyphicon-trash",
+              displayName: "Delete"
+            }
+          ]
+        }
+      ]
     },
     imo: {
       freeText: false,
