93b2baa8001008ce006a50a704b7308730dd60c8
soffia-dev <74037613+soffia-dev@users.noreply.github.com>
2020-11-11 19:46:58 +0530

    ED-5361: Css updates

--- playground/app/dx/uiSchema.js
+++ playground/app/dx/uiSchema.js
@@ -1,16 +1,59 @@
 export default {
   diagnosis: {
-    classNames: "col-md-12",
-    "ui:field": "table",
+    classNames: 'col-md-12 ',
+    'ui:field': 'table',
     table: {
       tableCols: [
-        { dataField: "description", editable: false },
-        { dataField: "icd10", editable: false },
-        { dataField: "icd9", editable: false },
+        {
+          dataField: 'icd10',
+          editable: false,
+          className: 'col-md-2 rxntTableStyle rxntHeaderStyle',
+          columnClassName: 'col-md-2 rxntTableStyle rxntRowStyle',
+        },
+        {
+          dataField: 'description',
+          editable: false,
+          className: 'col-md-7 rxntTableStyle rxntHeaderStyle',
+          columnClassName: 'col-md-7 rxntTableStyle rxntRowStyle',
+        },
+
+        {
+          dataField: 'snomed',
+          editable: false,
+          className: 'col-md-2 rxntTableStyle rxntHeaderStyle',
+          columnClassName: 'col-md-2 rxntTableStyle rxntRowStyle',
+        }
       ],
+      rightActions: [
+        {
+          action: "inheritedAction",
+          className: "col-md-1",
+          columnClassName: "col-md-1",
+          editColumnClassName: "col-md-1",
+          icon: "glyphicon glyphicon-option-vertical",
+          inheritedAction: [
+            {
+              action: "edit",
+              className: "col-md-1",
+              columnClassName: "col-md-1",
+              editColumnClassName: "col-md-1",
+              icon: "glyphicon glyphicon-pencil",
+              displayName: "Edit"
+            },
+            {
+              action: "delete",
+              className: "col-md-1",
+              columnClassName: "col-md-1",
+              editColumnClassName: "col-md-1",
+              icon: "glyphicon glyphicon-trash",
+              displayName: "Delete"
+            }
+          ]
+        }
+      ]
     },
     imo: {
       freeText: false,
     },
   },
-};
+}
