db50000c740b71dc026c05341ee55180aca55e01
soffia-dev <74037613+soffia-dev@users.noreply.github.com>
2020-11-20 05:46:33 +0530

    ED-5361: Update table UI for coding and Diagnosis (#174)
    ED-5361: Change Column width and update the css for coding and diagnosis

--- playground/app/dx/formData.js
+++ playground/app/dx/formData.js
@@ -1,3 +1,36 @@
-const formData = {};
-
-export default formData;
+export default {
+  diagnosis: [
+    {
+      patientDiagnosisId: 5016621,
+      description: "Struck by macaw, subsequent encounter",
+      icd10: "W61.12XD",
+      icd10Description: "Struck by macaw, subsequent encounter",
+      icd9: "V58.89",
+      icd9Description: "V58.89",
+      onSetDate: "2018-08-29",
+      statusDate: "2018-08-29",
+      diagnosedDate: "2018-08-29",
+      status: "1",
+      type: "0",
+      snomed: "418589001",
+      snomedDescription: "Accidental physical contact with animal",
+      isSelected: true
+    },
+    {
+      patientDiagnosisId: 5016622,
+      description: "demo",
+      icd10: "W61.12XD",
+      icd10Description: "Struck by macaw, subsequent encounter",
+      icd9: "V58.89",
+      icd9Description: "V58.89",
+      onSetDate: "2018-08-29",
+      statusDate: "2018-08-29",
+      diagnosedDate: "2018-08-29",
+      status: "1",
+      type: "0",
+      snomed: "418589001",
+      snomedDescription: "Accidental physical contact with animal",
+      isSelected: true
+    }
+  ]
+};
--- playground/app/dx/uiSchema.js
+++ playground/app/dx/uiSchema.js
@@ -1,50 +1,52 @@
 export default {
   diagnosis: {
-    classNames: 'col-md-12 ',
-    'ui:field': 'table',
+    classNames: "col-md-12 ",
+    "ui:field": "table",
     table: {
       tableCols: [
         {
-          dataField: 'icd10',
+          dataField: "icd10",
           editable: false,
-          className: 'col-md-2 rxntTableStyle rxntHeaderStyle',
-          columnClassName: 'col-md-2 rxntTableStyle rxntRowStyle',
+          className: "col-md-1 rxntTableStyle rxntHeaderStyle rxntBorderStyle",
+          columnClassName:
+            "col-md-1 rxntTableStyle rxntRowStyle rxntBorderStyle"
         },
         {
-          dataField: 'description',
+          dataField: "description",
           editable: false,
-          className: 'col-md-7 rxntTableStyle rxntHeaderStyle',
-          columnClassName: 'col-md-7 rxntTableStyle rxntRowStyle',
+          className: "col-md-9 rxntTableStyle rxntHeaderStyle rxntBorderStyle",
+          columnClassName:
+            "col-md-9 rxntTableStyle rxntRowStyle rxntBorderStyle"
         },
 
         {
-          dataField: 'snomed',
+          dataField: "snomed",
           editable: false,
-          className: 'col-md-2 rxntTableStyle rxntHeaderStyle',
-          columnClassName: 'col-md-2 rxntTableStyle rxntRowStyle',
+          className: "col-md-1 rxntTableStyle rxntHeaderStyle",
+          columnClassName: "col-md-1 rxntTableStyle rxntRowStyle"
         }
       ],
       rightActions: [
         {
-          action: "inheritedAction",
-          className: "col-md-1",
-          columnClassName: "col-md-1",
-          editColumnClassName: "col-md-1",
+          action: "dropDownAction",
+          className: "table-action rxntTableStyle rxntHeaderStyle",
+          columnClassName: "table-action rxntTableStyle rxntRowStyle ",
+          editColumnClassName: "table-action",
           icon: "glyphicon glyphicon-option-vertical",
-          inheritedAction: [
+          dropDownAction: [
             {
               action: "edit",
-              className: "col-md-1",
-              columnClassName: "col-md-1",
-              editColumnClassName: "col-md-1",
+              className: "table-action",
+              columnClassName: "table-action",
+              editColumnClassName: "table-action",
               icon: "glyphicon glyphicon-pencil",
               displayName: "Edit"
             },
             {
               action: "delete",
-              className: "col-md-1",
-              columnClassName: "col-md-1",
-              editColumnClassName: "col-md-1",
+              className: "table-action",
+              columnClassName: "table-action",
+              editColumnClassName: "table-action",
               icon: "glyphicon glyphicon-trash",
               displayName: "Delete"
             }
@@ -53,7 +55,7 @@ export default {
       ]
     },
     imo: {
-      freeText: false,
-    },
-  },
-}
+      freeText: false
+    }
+  }
+};
